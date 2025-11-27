add8b62d244f72634b1a027cd898d838bd373081
Sergej <sergey@walrus.lv>
2025-11-27 18:14:11 +0200

    First commit

--- src/test/java/walee/pages/main/EnglishPageLight.java
+++ src/test/java/walee/pages/main/EnglishPageLight.java
@@ -1,9 +1,9 @@
-package walee.pages;
+package walee.pages.main;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class EnglishPage {
+public class EnglishPageLight {
     private WebDriver driver;
 
    private By engLangBtn = By.xpath("//a[@class='langBtn' and @data-lang='en']");
@@ -12,7 +12,7 @@ public class EnglishPage {
 
 
 
-    public EnglishPage (WebDriver driver) {
+    public EnglishPageLight(WebDriver driver) {
         this.driver = driver;
     }
 
